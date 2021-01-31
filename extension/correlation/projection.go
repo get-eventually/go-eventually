e6b4556652909280ebaf6c280d3efe6890af2d19
Danilo Cianfrone <danilocianfr@gmail.com>
2021-02-01 00:50:23 +0100

    doc(correlation): add package documentation (#9)

--- extension/correlation/projection.go
+++ extension/correlation/projection.go
@@ -9,14 +9,24 @@ import (
 
 var _ projection.Applier = ProjectionWrapper{}
 
+// ProjectionWrapper is an extension component that adds Correlation
+// and Causation ids to the context of the underlying projection.Applier
+// instance, if found in the Message received in Apply.
+//
+// Use WrapProjection to create a new instance.
 type ProjectionWrapper struct {
 	applier projection.Applier
 }
 
+// WrapProjection wraps the specified projection.Applier instance
+// with a ProjectionWrapper extension component.
 func WrapProjection(applier projection.Applier) ProjectionWrapper {
 	return ProjectionWrapper{applier: applier}
 }
 
+// Apply applies the provided Event to the wrapped projection.Applier,
+// using an augmented context containing Correlation and Causation ids
+// in the Event Metadata, if any.
 func (pw ProjectionWrapper) Apply(ctx context.Context, event eventstore.Event) error {
 	if correlationID, ok := event.Metadata[CorrelationIDKey].(string); ok {
 		ctx = WithCorrelationID(ctx, correlationID)
