9599e4e072b30689c38ee0ffb9e7f98e4787d21b
Danilo Cianfrone <danilocianfr@gmail.com>
2021-02-12 00:11:41 +0100

    feat(projection): add documentation and testing, plus Runner implementation

---
+++ projection/doc.go
@@ -0,0 +1,3 @@
+// Package projection contains a Projection interface, one for left-folding
+// events to execute side effects, and one for Domain Read Models.
+package projection
--- projection/projection.go
+++ projection/projection.go
@@ -2,16 +2,103 @@ package projection
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/eventually-rs/eventually-go/eventstore"
 	"github.com/eventually-rs/eventually-go/query"
+	"github.com/eventually-rs/eventually-go/subscription"
+
+	"golang.org/x/sync/errgroup"
 )
 
+// Applier is a segregated Projection interface that focuses on applying
+// Events to a Projection state to trigger a side effect.
+//
+// Applier implementations do not necessarily require to be Domain Read Models,
+// and thus implementing the query.Handler interface.
+//
+// Examples of pure Appliers are Process Managers, which are long running
+// Event listeners that trigger side effects by orchestrating compensating actions.
+//
+// Use a Runner with a tailored Subscription type to make use
+// of an Applier instance.
 type Applier interface {
 	Apply(context.Context, eventstore.Event) error
 }
 
+// ApplierFunc is a functional Applier type implementation.
+type ApplierFunc func(ctx context.Context, event eventstore.Event) error
+
+// Apply executes the function pointed by the current value.
+func (fn ApplierFunc) Apply(ctx context.Context, event eventstore.Event) error { return fn(ctx, event) }
+
+// Projection represents a Domain Read Model, whose state is updated
+// through means of persisted Events coming form the Event Store (the Applier interface),
+// and can be accessed using a Domain Query (the query.Handler interface).
+//
+// Use a Runner with a tailored Subscription type to make use of a Projection.
+// Usually, if long-lived and stored in persistent storage, it's suggested to use
+// a Catch-up Subscription with a Projection, using a persisted Checkpointer
+// to survive application restarts.
 type Projection interface {
 	query.Handler
 	Applier
+}
+
+// DefaultRunnerBufferSize is the default size for the buffered channels
+// opened by a Runner instance, if not specified.
+const DefaultRunnerBufferSize = 32
+
+// Runner is an infrastructural component that orchestrates the state update
+// of an Applier or a Projection using the provided Subscription,
+// to subscribe to incoming events from the Event Store.
+type Runner struct {
+	Applier      Applier
+	Subscription subscription.Subscription
+	BufferSize   int
+}
+
+// Run starts listening to Events from the provided Subscription
+// and sinking them to the Applier instance to trigger state change.
+//
+// Run is a blocking call, that will exit when either the Applier returns an error,
+// or the Subscription stops.
+//
+// Run uses buffered channels to coordinate events communication between components,
+// using the value specified in BufferSize, if any, or DefaultRunnerBufferSize otherwise.
+func (r Runner) Run(ctx context.Context) error {
+	if r.BufferSize == 0 {
+		r.BufferSize = DefaultRunnerBufferSize
+	}
+
+	eventStream := make(chan eventstore.Event, r.BufferSize)
+	toCheckpoint := make(chan eventstore.Event, r.BufferSize)
+
+	group, ctx := errgroup.WithContext(ctx)
+
+	group.Go(func() error {
+		return r.Subscription.Start(ctx, eventStream)
+	})
+
+	group.Go(func() error {
+		defer close(toCheckpoint)
+
+		for event := range eventStream {
+			if err := r.Applier.Apply(ctx, event); err != nil {
+				return fmt.Errorf("projection.Runner: failed to apply event to projection: %w", err)
+			}
+
+			toCheckpoint <- event
+		}
+
+		return nil
+	})
+
+	for event := range toCheckpoint {
+		if err := r.Subscription.Checkpoint(ctx, event); err != nil {
+			return fmt.Errorf("projection.Runner: failed to checkpoint event: %w", err)
+		}
+	}
+
+	return nil
 }
