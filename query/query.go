44f7eae9075b2fd3a03e8c09e5f4d30f76b870ff
Danilo Cianfrone <danilocianfr@gmail.com>
2021-02-16 00:16:34 +0100

    doc(query): add documentation

--- query/query.go
+++ query/query.go
@@ -2,19 +2,20 @@ package query
 
 import "context"
 
+// Query is a marker interface for Domain Query types.
 type Query interface{}
 
+// Answer is a marker interface for answers to Domain Queries, returned
+// by a Query Handler.
 type Answer interface{}
 
-type Dispatcher interface {
-	Dispatch(context.Context, Query) (Answer, error)
-}
-
+// Handler is a component capable of handling Domain Queries, resolving
+// them into an Answer, typically a Domain Read Model.
+//
+// An Handler implementation specifies their supported Query type
+// using the QueryType method. Please, make sure the type returned by this method
+// reflects the same type of the Query submitted in your application.
 type Handler interface {
 	QueryType() Query
 	Handle(context.Context, Query) (Answer, error)
 }
-
-type HandlerFunc func(context.Context, Query) (Answer, error)
-
-func (fn HandlerFunc) Handle(ctx context.Context, q Query) (Answer, error) { return fn(ctx, q) }
