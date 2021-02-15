434ecb341015c423a8370423ec525883aafdf229
Danilo Cianfrone <danilocianfr@gmail.com>
2021-02-16 00:28:41 +0100

    feat(query): add documentation and testing (#19)
    * doc(query): add documentation
    
    * test(query): add tests and complete coverage

--- query/bus.go
+++ query/bus.go
@@ -6,19 +6,53 @@ import (
 	"reflect"
 )
 
+// Dispatcher represents a component that routes Domain Queries into
+// their appropriate Query Handlers.
+//
+// Differently from Command Handlers, Query Handlers return an Answer to
+// the Domain Query dispatched, which usually is the Domain Read Model
+// for a specific query type.
+//
+// Implementations can be synchronous or asynchronous, although it is generally
+// used as a synchronous component, i.e. the call to Dispatch returns when
+// the query has been resolved.
+type Dispatcher interface {
+	Dispatch(context.Context, Query) (Answer, error)
+}
+
+// SimpleBus is a simple Query Bus implementation, synchronous and that
+// works in-memory by registering Query Handlers and dispatching
+// Domain Queries, the Bus will take care of routing the Query to the
+// appropriate Query Handler.
+//
+// Use NewSimpleBus to create a new SimpleBus instance.
 type SimpleBus struct {
 	handlers map[reflect.Type]Handler
 }
 
+// NewSimpleBus returns a new instance of SimpleBus.
 func NewSimpleBus() *SimpleBus {
 	return &SimpleBus{handlers: make(map[reflect.Type]Handler)}
 }
 
+// Register adds the specified Query Handler to the SimpleBus routing table,
+// associated with the Query Handler's Domain Query type, so that calls
+// to Dispatch using the specified Domain Query type will be routed
+// to this Query Handler.
+//
+// Please note, when registering multiple Handlers accepting the same Query type,
+// the last registration call will overwrite any previous Handler registration,
+// as SimpleBus, intentionally, does not support any kind of voting mechanism.
 func (b *SimpleBus) Register(handler Handler) {
 	typ := reflect.TypeOf(handler.QueryType())
 	b.handlers[typ] = handler
 }
 
+// Dispatch routes the provided Domain Query to the appropriate Query Handler
+// registered in the Bus, if any, and returns the produced Answer.
+//
+// If the submitted Query has not been registered by any Handler, an
+// error will be returned instead.
 func (b SimpleBus) Dispatch(ctx context.Context, query Query) (Answer, error) {
 	typ := reflect.TypeOf(query)
 
---
+++ query/bus_test.go
@@ -0,0 +1,53 @@
+package query_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/eventually-rs/eventually-go/query"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type domainQuery struct {
+	name  string
+	value int64
+}
+
+type queryHandler struct{}
+
+func (queryHandler) QueryType() query.Query { return domainQuery{} }
+
+func (queryHandler) Handle(ctx context.Context, q query.Query) (query.Answer, error) {
+	return q.(domainQuery).value + 1, nil
+}
+
+func TestSimpleBus(t *testing.T) {
+	t.Run("bus fails if query has not been registered", func(t *testing.T) {
+		ctx := context.Background()
+		queryBus := query.NewSimpleBus()
+
+		answer, err := queryBus.Dispatch(ctx, domainQuery{
+			name:  "fail-query",
+			value: 1,
+		})
+
+		assert.Nil(t, answer)
+		assert.Error(t, err)
+	})
+
+	t.Run("bus returns answer if domain query is registered", func(t *testing.T) {
+		ctx := context.Background()
+		queryBus := query.NewSimpleBus()
+
+		queryBus.Register(queryHandler{})
+
+		answer, err := queryBus.Dispatch(ctx, domainQuery{
+			name:  "fail-query",
+			value: 1,
+		})
+
+		assert.Equal(t, int64(2), answer)
+		assert.NoError(t, err)
+	})
+}
