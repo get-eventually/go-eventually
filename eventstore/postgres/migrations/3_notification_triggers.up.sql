428def202a888c4a8e055af394afba2dffdd682a
Danilo Cianfrone <danilocianfr@gmail.com>
2021-05-09 10:29:32 +0200

    fix(postgres): migrate notification to include sequence_number

--- eventstore/postgres/migrations/3_notification_triggers.up.sql
+++ eventstore/postgres/migrations/3_notification_triggers.up.sql
@@ -1,20 +1,16 @@
 CREATE OR REPLACE FUNCTION notify_all()
 RETURNS TRIGGER
 AS $$
-DECLARE
-    metadata JSONB;
 BEGIN
-    metadata = NEW.metadata ||
-        ('{"Global-Sequence-Number":' || NEW.global_sequence_number || ' }')::JSONB;
-
     PERFORM pg_notify('$all', ''
         || '{'
-        || '"stream_id": "'      || NEW.stream_id           || '" ,'
-        || '"stream_type": "'    || NEW.stream_type         || '" ,'
-        || '"event_type": "'     || NEW.event_type          || '" ,'
-        || '"version": '         || NEW."version"           || ', '
-        || '"event": '           || NEW."event"::TEXT       || ', '
-        || '"metadata": '        || metadata
+        || '"stream_id": "'       || NEW.stream_id              || '" ,'
+        || '"stream_type": "'     || NEW.stream_type            || '" ,'
+        || '"event_type": "'      || NEW.event_type             || '" ,'
+        || '"sequence_number": "' || NEW.global_sequence_number || '" ,'
+        || '"version": '          || NEW."version"              || ', '
+        || '"event": '            || NEW."event"::TEXT          || ', '
+        || '"metadata": '         || NEW.metadata
         || '}');
 
     RETURN NEW;
@@ -24,20 +20,16 @@ $$ LANGUAGE PLPGSQL;
 CREATE OR REPLACE FUNCTION notify_stream_type()
 RETURNS TRIGGER
 AS $$
-DECLARE
-    metadata JSONB;
 BEGIN
-    metadata = NEW.metadata ||
-        ('{"Global-Sequence-Number":' || NEW.global_sequence_number || ' }')::JSONB;
-
     PERFORM pg_notify(NEW.stream_type, ''
         || '{'
-        || '"stream_id": "'      || NEW.stream_id           || '" ,'
-        || '"stream_type": "'    || NEW.stream_type         || '" ,'
-        || '"event_type": "'     || NEW.event_type          || '" ,'
-        || '"version": '         || NEW."version"           || ', '
-        || '"event": '           || NEW."event"::TEXT       || ', '
-        || '"metadata": '        || metadata
+        || '"stream_id": "'       || NEW.stream_id              || '" ,'
+        || '"stream_type": "'     || NEW.stream_type            || '" ,'
+        || '"event_type": "'      || NEW.event_type             || '" ,'
+        || '"sequence_number": "' || NEW.global_sequence_number || '" ,'
+        || '"version": '          || NEW."version"              || ', '
+        || '"event": '            || NEW."event"::TEXT          || ', '
+        || '"metadata": '         || NEW.metadata
         || '}');
 
     RETURN NEW;
