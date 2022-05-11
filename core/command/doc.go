ee1f26e6a2924b6ad2ecff5930918f8ec31516c2
Danilo Cianfrone <danilocianfr@gmail.com>
2022-05-11 17:51:05 +0200

    feat(core/command): add some helper functions and documentation (#86)

---
+++ core/command/doc.go
@@ -0,0 +1,4 @@
+// Package command contains types and interfaces for implementing Command Handlers,
+// necessary for producing side effects in your Aggregates and system,
+// and implement your Domain's business logic.
+package command
