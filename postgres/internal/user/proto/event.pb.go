e3858a6cf4d3003459d80d027f80674b8fc499a5
Danilo Cianfrone <danilocianfr@gmail.com>
2022-10-23 20:27:33 +0200

    fix: gci config

--- postgres/internal/user/proto/event.pb.go
+++ postgres/internal/user/proto/event.pb.go
@@ -7,11 +7,12 @@
 package proto
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	date "google.golang.org/genproto/googleapis/type/date"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
