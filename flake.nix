9540ad1e26cda3712b74d0275610c7741350faa0
Danilo Cianfrone <6481571+ar3s3ru@users.noreply.github.com>
2024-03-19 14:39:10 +0100

    fix: release pipeline with release-please (#218)
    * feat: add direnv
    
    * fix: move release-type inside components

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+
+          # NOTE: fixing Go 1.21 for the project.
+          go = pkgs.go_1_21;
+        in
+        {
+          devShells.default = with pkgs; mkShell {
+            nativeBuildInputs = [
+              go
+              buf
+            ];
+
+            packages = [
+              gopls
+              gotools
+              go-outline
+              gopkgs
+              delve
+
+              # Linters
+              nil
+              golangci-lint
+            ];
+
+            # Provide binary paths for tooling through environment variables.
+            GO_BIN_PATH = "${go}/bin/go";
+            GOPLS_PATH = "${gopls}/bin/gopls";
+            DLV_PATH = "${delve}/bin/dlv";
+
+            # Disable ryuk container for testcontainers.
+            TESTCONTAINERS_RYUK_DISABLED = "true";
+          };
+        }
+      );
+}
