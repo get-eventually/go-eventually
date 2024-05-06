72b0f97e87c5acd39a9ff940492324189a9027a2
Danilo Cianfrone <danilocianfr@gmail.com>
2024-05-06 11:15:13 +0200

    fix(query): linter complaints

--- query/scenario_test.go
+++ query/scenario_test.go
@@ -25,7 +25,7 @@ func TestScenario(t *testing.T) {
 		BirthDate: time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC),
 	}
 
-	makeQueryHandler := func(es event.Store) *user.GetByEmailHandler {
+	makeQueryHandler := func(_ event.Store) *user.GetByEmailHandler {
 		return user.NewGetByEmailHandler()
 	}
 
